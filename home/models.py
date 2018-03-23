0d14d227f48e80beb98e503752a9ee0f2a946af9
patrik-drean <patrikdrean@gmail.com>
2018-03-23 14:28:48 -0600

    added box shadowing and resized some divs

--- home/models.py
+++ home/models.py
@@ -13,6 +13,7 @@ class Blog(models.Model):
     filename = models.TextField(blank=True, null=True)
     alt_text = models.TextField(blank=True, null=True)
     top_post = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def image_url(self):
         blog = Blog.objects.get(id = self.id)
@@ -22,6 +23,8 @@ class Blog(models.Model):
 class Category(models.Model):
     name = models.TextField()
     filename = models.TextField(blank=True, null=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    last_modified_date = models.DateTimeField(auto_now=True)
 
     def image_url(self):
         category = Category.objects.get(id = self.id)
--- home/scripts/index.js
+++ home/scripts/index.js
@@ -1,25 +1,38 @@
 $(function(context) {
    return function() {
 
-
       var top_blogs = $('.top_blog_div');
       var recent_blogs = $('.recent_blog_div');
       hover_div(top_blogs);
       hover_div(recent_blogs);
-
-
 }
 
 }(DMP_CONTEXT.get()))
 
 
+// function hover_div(divs) {
+//
+//    divs.mouseenter(function(event) {
+//    $(this).addClass('shadow');
+//    $(this).addClass('shadow_text');
+//    });
+//    divs.mouseleave(function(event) {
+//    $(this).removeClass('shadow');
+//    $(this).removeClass('shadow_text');
+//    });
+//
+//
+// }
 function hover_div(divs) {
 
    divs.mouseenter(function(event) {
-
-   main_img.attr('src', this.src);
+   $(this).addClass('shadow');
+   $(this).find('h3').addClass('shadow_text');
    });
-
+   divs.mouseleave(function(event) {
+   $(this).removeClass('shadow');
+   $(this).find('h3').removeClass('shadow_text');
+   });
 
 
 }
