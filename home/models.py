a97864b631d4fc0c7b7e408388c483ddc97d9141
patrik-drean <patrikdrean@gmail.com>
2018-03-24 15:12:30 -0600

    migrate

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class User(User):
-    pass
+# class User(User):
+#     pass
 
 class Category(models.Model):
     name = models.TextField()
