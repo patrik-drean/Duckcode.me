525ad4f2d4001dd632eee90e8392daf65d064489
patrik-drean <patrikdrean@gmail.com>
2018-03-23 20:03:39 -0600

    disabled local db

--- duck_code/settings.py
+++ duck_code/settings.py
@@ -98,16 +98,16 @@ WSGI_APPLICATION = 'duck_code.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'duck_code',
-        'USER': 'patrikdrean',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'duck_code',
+#         'USER': 'patrikdrean',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 ################# Turn this off when working locally #################
 import dj_database_url
 
