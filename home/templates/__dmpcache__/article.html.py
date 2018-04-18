3104317a9b793698b92e7caa9dc55f8a7f199a03
patrik-drean <patrikdrean@gmail.com>
2018-04-18 10:08:42 -0600

    updated migrations

--- home/templates/__dmpcache__/article.html.py
+++ home/templates/__dmpcache__/article.html.py
@@ -5,13 +5,13 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1523714941.6349282
+_modified_time = 1524067692.899325
 _enable_loop = True
 _template_filename = '/Users/patrikdrean/Documents/python_projects/duck_code/duck_code/home/templates/article.html'
 _template_uri = 'article.html'
 _source_encoding = 'utf-8'
 import django_mako_plus
-_exports = ['title', 'top_content', 'middle_content']
+_exports = ['title', 'meta_description', 'top_content', 'middle_content']
 
 
 def _mako_get_namespace(context, name):
@@ -29,18 +29,25 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        article = context.get('article', UNDEFINED)
         def top_content():
             return render_top_content(context._locals(__M_locals))
-        def title():
-            return render_title(context._locals(__M_locals))
         def middle_content():
             return render_middle_content(context._locals(__M_locals))
-        article = context.get('article', UNDEFINED)
+        def title():
+            return render_title(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def meta_description():
+            return render_meta_description(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
             context['self'].title(**pageargs)
+        
+
+        __M_writer('\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'meta_description'):
+            context['self'].meta_description(**pageargs)
         
 
         __M_writer('\n\n')
@@ -72,13 +79,28 @@ def render_title(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_meta_description(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        article = context.get('article', UNDEFINED)
+        def meta_description():
+            return render_meta_description(context)
+        __M_writer = context.writer()
+        __M_writer('\n  ')
+        __M_writer(str( article.meta_description))
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 def render_top_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         def top_content():
             return render_top_content(context)
         article = context.get('article', UNDEFINED)
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n   <img id="main_img" src="')
         __M_writer(str( STATIC_URL ))
@@ -94,9 +116,9 @@ def render_top_content(context,**pageargs):
 def render_middle_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
+        article = context.get('article', UNDEFINED)
         def middle_content():
             return render_middle_content(context)
-        article = context.get('article', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n   <h1>')
         __M_writer(str( article.title ))
@@ -112,6 +134,6 @@ def render_middle_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "/Users/patrikdrean/Documents/python_projects/duck_code/duck_code/home/templates/article.html", "uri": "article.html", "source_encoding": "utf-8", "line_map": {"28": 0, "41": 1, "46": 3, "51": 8, "56": 18, "62": 3, "69": 3, "75": 5, "83": 5, "84": 6, "85": 6, "86": 6, "87": 6, "88": 6, "94": 9, "101": 9, "102": 10, "103": 10, "104": 12, "105": 12, "106": 14, "107": 14, "113": 107}}
+{"filename": "/Users/patrikdrean/Documents/python_projects/duck_code/duck_code/home/templates/article.html", "uri": "article.html", "source_encoding": "utf-8", "line_map": {"28": 0, "43": 1, "48": 3, "53": 6, "58": 11, "63": 21, "69": 3, "76": 3, "82": 4, "89": 4, "90": 5, "91": 5, "97": 8, "105": 8, "106": 9, "107": 9, "108": 9, "109": 9, "110": 9, "116": 12, "123": 12, "124": 13, "125": 13, "126": 15, "127": 15, "128": 17, "129": 17, "135": 129}}
 __M_END_METADATA
 """
