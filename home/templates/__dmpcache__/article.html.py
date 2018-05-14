ef224c767c6933813d989f9874809999bd08ec33
patrik-drean <patrikdrean@gmail.com>
2018-05-14 17:49:10 -0600

    fortnite blog

--- home/templates/__dmpcache__/article.html.py
+++ home/templates/__dmpcache__/article.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1525458339.955673
+_modified_time = 1526341724.932586
 _enable_loop = True
 _template_filename = '/Users/patrikdrean/Documents/python_projects/duck_code/duck_code/home/templates/article.html'
 _template_uri = 'article.html'
@@ -29,16 +29,16 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def title():
-            return render_title(context._locals(__M_locals))
         def top_content():
             return render_top_content(context._locals(__M_locals))
         def middle_content():
             return render_middle_content(context._locals(__M_locals))
-        article = context.get('article', UNDEFINED)
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         def meta_description():
             return render_meta_description(context._locals(__M_locals))
+        def title():
+            return render_title(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        article = context.get('article', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
@@ -71,9 +71,9 @@ def render_body(context,**pageargs):
 def render_title(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        article = context.get('article', UNDEFINED)
         def title():
             return render_title(context)
+        article = context.get('article', UNDEFINED)
         __M_writer = context.writer()
         __M_writer(str( article.nav_title))
         return ''
@@ -99,10 +99,10 @@ def render_meta_description(context,**pageargs):
 def render_top_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        article = context.get('article', UNDEFINED)
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         def top_content():
             return render_top_content(context)
+        article = context.get('article', UNDEFINED)
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         __M_writer = context.writer()
         __M_writer("\n   <img id='main_img' src='")
         __M_writer(str( STATIC_URL ))
@@ -126,7 +126,7 @@ def render_middle_content(context,**pageargs):
         __M_writer(str( article.title ))
         __M_writer("</h1>\n   <p id='article_author'>by Patrik Drean</p>\n   <p id='article_datestamp'>")
         __M_writer(str( article.create_date.strftime('%B %d, %Y') ))
-        __M_writer("</p>\n\n   <div id='article_content'>\n     ")
+        __M_writer("</p>\n\n   <div id='article_content'>\n\n     ")
         __M_writer(str( article.content ))
         __M_writer('\n   </div>\n')
         return ''
@@ -136,6 +136,6 @@ def render_middle_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "/Users/patrikdrean/Documents/python_projects/duck_code/duck_code/home/templates/article.html", "uri": "article.html", "source_encoding": "utf-8", "line_map": {"28": 0, "43": 1, "48": 3, "53": 6, "58": 11, "63": 20, "64": 25, "65": 25, "71": 3, "78": 3, "84": 4, "91": 4, "92": 5, "93": 5, "99": 8, "107": 8, "108": 9, "109": 9, "110": 9, "111": 9, "112": 9, "118": 12, "125": 12, "126": 13, "127": 13, "128": 15, "129": 15, "130": 18, "131": 18, "137": 131}}
+{"filename": "/Users/patrikdrean/Documents/python_projects/duck_code/duck_code/home/templates/article.html", "uri": "article.html", "source_encoding": "utf-8", "line_map": {"28": 0, "43": 1, "48": 3, "53": 6, "58": 11, "63": 21, "64": 26, "65": 26, "71": 3, "78": 3, "84": 4, "91": 4, "92": 5, "93": 5, "99": 8, "107": 8, "108": 9, "109": 9, "110": 9, "111": 9, "112": 9, "118": 12, "125": 12, "126": 13, "127": 13, "128": 15, "129": 15, "130": 19, "131": 19, "137": 131}}
 __M_END_METADATA
 """
