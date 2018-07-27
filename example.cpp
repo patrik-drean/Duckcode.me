f193d92533f75983852eb40db016d7436183ace7
patrik-drean <patrikdrean@gmail.com>
2018-07-26 19:01:23 -0600

    13 - running c++ in terminal article

---
+++ example.cpp
@@ -0,0 +1,20 @@
+// Import library
+#include <iostream>
+using namespace std;
+
+// This is the main function. It returns '0' just as a standard.
+int main()
+{
+  int number;
+
+  // Write to console
+  cout << "Enter a number between 1 and 10: ";
+
+  // Assign input to the variable
+  cin >> number;
+
+  // Output
+  cout << "The value you entered is " << number;
+
+  return 0;
+}
